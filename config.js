8b4f1a0cc9f6a3705ba26d8e743a914fd1590acb
unknown <lj@55t.se>
2023-02-04 13:03:18 +0100

    Repliability updates

---
+++ config.js
@@ -0,0 +1,11 @@
+
+const botConfig = {
+   token: "TOKEN",
+   clientId: "852557080518852678",
+   globalCommands: true,
+   guilds: ["888876166570713108"], // You only need this if you want to register guild cmds instead of global ones
+   maxRamUsage: 1024, //MB
+   scheduledRestart: 0 // 0 = disabled, hours between restarts
+}
+
+module.exports = { botConfig }
